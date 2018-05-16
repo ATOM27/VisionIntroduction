a347e5f190faef958ca78b7ff35a3c7c72404a34
ATOM27 <eugene.mekhedov@gmail.com>
2018-05-16 18:47:31 +0300

    landmarks detection

--- VisionIntroduction/ViewController.swift
+++ VisionIntroduction/ViewController.swift
@@ -9,21 +9,30 @@
 import UIKit
 import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    //MARK: - Properties
+    var image = UIImage(named: "image")!
+    var imageView: UIImageView!
+
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        guard let image = UIImage(named: "aaa") else{ return }
-        let imageView = UIImageView(image: image)
+        // ImageView setup
+        imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFit
-        
         imageView.backgroundColor = .red
         
         let scaledHeight = view.frame.width / image.size.width * image.size.height
         imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
         self.view.addSubview(imageView)
         
+//        self.detectFace(imageView: self.imageView)
+        self.detectLandmarks(imageView: imageView)
+    }
+    
+    //MARK: - Vision
+    private func detectFace(imageView: UIImageView){
         let request = VNDetectFaceRectanglesRequest { (req, err) in
             
             if let err = err{
@@ -33,29 +42,118 @@ class ViewController: UIViewController {
             
             req.results?.forEach({ (res) in
                 guard let faceObservation = res as? VNFaceObservation else{ return }
-                print(faceObservation.boundingBox)
                 
-                let size = CGSize(width: faceObservation.boundingBox.width * imageView.bounds.width,
-                                  height: faceObservation.boundingBox.height * imageView.bounds.height)
-                let origin = CGPoint(x: faceObservation.boundingBox.origin.x * imageView.bounds.width,
-                                     y: (1 - faceObservation.boundingBox.origin.y) * imageView.bounds.height - size.height)
+                let rect = self.computeRectFromBoundingBox(boundingBox: faceObservation.boundingBox, inImageView: imageView)
+                self.drawRectangle(rect: rect)
+                self.drawHat(faceRect: rect)
                 
-                let redView = UIView()
-                redView.backgroundColor = UIColor.red
-                redView.alpha = 0.3
-                redView.frame = CGRect(origin: origin, size: size)
-                self.view.addSubview(redView)
                 
             })
         }
         
-        guard let cgImage = image.cgImage else{ return }
+        guard let cgImage = imageView.image?.cgImage else{ return }
         let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
         do{
             try handler.perform([request])
         }catch let error{
             print("Failed to perform request: \(error)")
         }
+    }
+    
+    private func detectLandmarks(imageView: UIImageView){
+        let faceRequest = VNDetectFaceLandmarksRequest { (req, err) in
+            if let err = err{
+                print("Failed to detect landmarks: \(err)")
+            }
+            
+            req.results?.forEach({ (res) in
+                guard let faceObservation = res as? VNFaceObservation else{ return }
+                
+                guard let landmarks = faceObservation.landmarks else{ return }
+                guard let normalizedPoints = landmarks.leftEye?.normalizedPoints else{ return }
+                var leftEyePoints = self.compute(normilizedPoints: normalizedPoints, inImageView: imageView)
+                
+                let path = UIBezierPath()
+                path.move(to: leftEyePoints.first!)
+                
+                leftEyePoints.removeFirst()
+                leftEyePoints.forEach({ (point) in
+                    path.addLine(to: point)
+                })
+                
+                path.close()
+                
+                let shapeLayer = CAShapeLayer()
+                shapeLayer.path = path.cgPath
+                shapeLayer.strokeColor = UIColor.green.cgColor
+                
+                imageView.layer.addSublayer(shapeLayer)
+            })
+        }
+        
+        guard let cgImage = imageView.image?.cgImage else{ return }
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try handler.perform([faceRequest])
+        } catch let err {
+            print("Failed to perform request: \(err)")
+        }
+    }
+    
+    //MARK: - Help methods
+    private func drawHat(faceRect: CGRect) {
+        let hat = #imageLiteral(resourceName: "Archer_Hat")
+
+        let hatSize = hat.size
+        let headSize = faceRect.size
+
+        let hatWidthForHead = (3.0 / 2.0) * headSize.width
+        let hatRatio = hatWidthForHead / hatSize.width
+        let scaleTransform = CGAffineTransform(scaleX: hatRatio,
+                                               y: hatRatio)
+        let adjustedHatSize = hatSize.applying(scaleTransform)
+
+        let hatRect = CGRect(
+            x: faceRect.midX - (adjustedHatSize.width / 2.0),
+            y: faceRect.minY - adjustedHatSize.height,
+            width: adjustedHatSize.width,
+            height: adjustedHatSize.height)
+
+        let imageView = UIImageView(image: hat)
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.cyan.cgColor
+        
+        imageView.frame = hatRect
+        self.view.addSubview(imageView)
+    }
+    
+    private func drawRectangle(rect: CGRect){
+        let redView = UIView()
+        redView.layer.borderColor = UIColor.red.cgColor
+        redView.layer.borderWidth = 2
+        
+        redView.frame = rect
+        
+        self.view.addSubview(redView)
+    }
+    
+    private func computeRectFromBoundingBox(boundingBox: CGRect, inImageView imageView: UIImageView) -> CGRect{
+        let size = CGSize(width: boundingBox.width * imageView.bounds.width,
+                          height: boundingBox.height * imageView.bounds.height)
+        let origin = CGPoint(x: boundingBox.origin.x * imageView.bounds.width,
+                             y: (1 - boundingBox.origin.y) * imageView.bounds.height - size.height)
+        return CGRect(origin: origin, size: size)
+    }
+    
+    private func compute(normilizedPoints: [CGPoint], inImageView imageView: UIImageView) -> [CGPoint]{
+        var points: [CGPoint] = []
+        
+        normilizedPoints.forEach { (normPoint) in
+            let point = CGPoint(x: (1 - normPoint.x) * imageView.bounds.width,
+                                y: ( 1 - normPoint.y) * imageView.bounds.height)
+            points.append(point)
+        }
+        return points
     }
 }
 
